e4793088adb31da497723e57292dbf1ab290a900
Cristóbal Contreras Rubio <crisconru@gmail.com>
2024-02-28 15:19:34 +0100

    zod-numbers: setting package main files

---
+++ schemas/zod/numbers/vitest.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    // ... Specify options here.
+    coverage: {
+      exclude: [
+        '**/*/constants.ts',
+        '**/*/types.ts',
+        'lib/*'
+      ]
+    }
+  },
+})
